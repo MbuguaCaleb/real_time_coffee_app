777331e669f0e775a52bcb336db4b861eda5e32f
MbuguaCaleb <mbuguacaleb30@gmail.com>
2020-09-22 18:36:57 +0300

    signout-functionality

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,10 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:real_time_coffee_app/services/auth.dart';
 
 class Home extends StatelessWidget {
+  /*remember that when i instantitate a class i can be able
+  to get the attributes and methods*/
+
+  final AuthService _auth = AuthService();
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('Home'),
+    return Scaffold(
+      backgroundColor: Colors.brown[50],
+      appBar: AppBar(
+        title: Text('Coffee App'),
+        backgroundColor: Colors.brown[400],
+        elevation: 0.0,
+        actions: <Widget>[
+          FlatButton.icon(
+              onPressed: () async {
+                //remember that after signing out from the stream
+                //i get a null
+                await _auth.signOut();
+              },
+              icon: Icon(Icons.person),
+              label: Text('logout'))
+        ],
+      ),
     );
   }
 }
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -2,15 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:real_time_coffee_app/models/user.dart';
 import 'package:real_time_coffee_app/screens/authentication/authenticate.dart';
+import 'package:real_time_coffee_app/screens/home/home.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    //return either Home or Authenticate Widget
     //The wrapper will be listening for authenticate changes and show either of the above
     final user = Provider.of<User>(context);
 
-    print(user);
-    return Authenticate();
+    //return either Home or Authenticate Widget
+    if (user == null) {
+      return Authenticate();
+    } else {
+      return Home();
+    }
   }
 }
